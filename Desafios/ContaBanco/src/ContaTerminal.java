fcb76373ad23c6c40c7ad8d1f8fb0d62463fafb3
Lucas-Romano-Pacheco <lucas_romano@id.uff.br>
2024-05-15 18:15:15 -0300

    ...

--- Desafios/ContaBanco/src/ContaTerminal.java
+++ Desafios/ContaBanco/src/ContaTerminal.java
@@ -5,7 +5,6 @@ public class ContaTerminal {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
-        //
 
         System.out.println("Digite o numero de sua conta bancaria: ");
         int numeroConta = scanner.nextInt();
